415e336f7f1296dbc5dc43dfca1ae13fdbcb35da
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-10-15 11:32:52 -0300

    changing API message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Olá mundo! Deploy na Heroku integrado com o GitHub';
+    return 'Olá mundo! Deploy automático com GitHub Actions!!!!!';
   }
 }
